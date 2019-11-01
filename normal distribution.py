aa0cdafdc183a90bdbfb88cb0161168ce6074822
Anshu Sobti <sobtianshu2@gmail.com>
2019-11-01 11:56:14 +0530

    Add files via upload

---
+++ normal distribution.py
@@ -0,0 +1,122 @@
+import numpy as np
+from matplotlib import pyplot
+#Setting the seed
+np.random.seed(100)
+#Generating random numbers
+data  = 5 * np.random.randn(100) +100
+data2 = np.random.uniform(0,10,100)
+data3 = np.array(list(data)+list(data2))
+
+#1. Histogram
+pyplot.hist(data)
+pyplot.show()
+
+pyplot.hist(data2)
+pyplot.show()
+
+pyplot.hist(data3)
+pyplot.show()
+
+#2. Q-Q Plot
+from statsmodels.graphics.gofplots import qqplot
+qqplot(data, line='s')
+pyplot.show()
+
+qqplot(data2, line='s')
+pyplot.show()
+
+qqplot(data3, line='s')
+pyplot.show()
+
+#3. Shapiro-Wilk Test
+from scipy.stats import shapiro
+stat, p = shapiro(data)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+alpha = 0.05
+if p > alpha:
+	print('Sample looks Gaussian (fail to reject H0)')
+else:
+	print('Sample does not look Gaussian (reject H0)')
+    
+
+stat, p = shapiro(data2)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+alpha = 0.05
+if p > alpha:
+	print('Sample looks Gaussian (fail to reject H0)')
+else:
+	print('Sample does not look Gaussian (reject H0)')
+    
+# normality test
+stat, p = shapiro(data3)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+alpha = 0.05
+if p > alpha:
+	print('Sample looks Gaussian (fail to reject H0)')
+else:
+	print('Sample does not look Gaussian (reject H0)')
+
+#4. D’Agostino’s K^2 Test
+from scipy.stats import normaltest
+stat, p = normaltest(data)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+alpha = 0.05
+if p > alpha:
+	print('Sample looks Gaussian (fail to reject H0)')
+else:
+	print('Sample does not look Gaussian (reject H0)')
+    
+stat, p = normaltest(data2)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+alpha = 0.05
+if p > alpha:
+	print('Sample looks Gaussian (fail to reject H0)')
+else:
+	print('Sample does not look Gaussian (reject H0)')
+    
+stat, p = normaltest(data3)
+print('Statistics=%.3f, p=%.3f' % (stat, p))
+# interpret
+alpha = 0.05
+if p > alpha:
+	print('Sample looks Gaussian (fail to reject H0)')
+else:
+	print('Sample does not look Gaussian (reject H0)')
+    
+#5. Anderson Darling Test
+from scipy.stats import anderson
+result = anderson(data)
+print('Statistic: %.3f' % result.statistic)
+p = 0
+for i in range(len(result.critical_values)):
+	sl, cv = result.significance_level[i], result.critical_values[i]
+	if result.statistic < result.critical_values[i]:
+		print('%.3f: %.3f, data looks normal (fail to reject H0)' % (sl, cv))
+	else:
+		print('%.3f: %.3f, data does not look normal (reject H0)' % (sl, cv))
+        
+result = anderson(data2)
+print('Statistic: %.3f' % result.statistic)
+p = 0
+for i in range(len(result.critical_values)):
+	sl, cv = result.significance_level[i], result.critical_values[i]
+	if result.statistic < result.critical_values[i]:
+		print('%.3f: %.3f, data looks normal (fail to reject H0)' % (sl, cv))
+	else:
+		print('%.3f: %.3f, data does not look normal (reject H0)' % (sl, cv))
+        
+result = anderson(data3)
+print('Statistic: %.3f' % result.statistic)
+p = 0
+for i in range(len(result.critical_values)):
+	sl, cv = result.significance_level[i], result.critical_values[i]
+	if result.statistic < result.critical_values[i]:
+		print('%.3f: %.3f, data looks normal (fail to reject H0)' % (sl, cv))
+	else:
+		print('%.3f: %.3f, data does not look normal (reject H0)' % (sl, cv))
+        
